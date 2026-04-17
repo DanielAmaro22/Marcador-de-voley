b35e440bcebd6554de72599fbe809d33c0646750
DanielAmaro22 <cdanielamaro22@gmail.com>
2026-04-17 10:41:52 -0300

    Add score tracking and point management functions
    Initialize score tracking and implement point management functions.

---
+++ script.js
@@ -0,0 +1,39 @@
+let scoreA = 0
+let scoreB = 0
+
+function addPoint(team){
+  if (scoreA != 25 && scoreB !=25) {
+    if (team === 'A'){
+      scoreA ++;
+      document.getElementById("scoreA").textContent = scoreA;
+    }
+     else if (team === 'B'){
+      scoreB ++;
+      document.getElementById("scoreB").textContent = scoreB;
+    }
+  }
+  else if (scoreA === 25){
+    alert ("Ganador team A")
+  }
+  else if (scoreB === 25){
+    alert ("Ganador team B")
+  }
+}
+
+function subtractPoint(team){
+  if (team === "A" && scoreA > 0){
+    scoreA --;
+    document.getElementById("scoreA").textContent= scoreA
+  }
+  else if (team === "B" && scoreB > 0){
+    scoreB --;
+    document.getElementById("scoreB").textContent= scoreB
+  }  
+}
+
+function resetScore(){
+  scoreA = 0;
+  scoreB = 0;
+  document.getElementById("scoreA").textContent= scoreA;
+  document.getElementById("scoreB").textContent= scoreB;
+}
