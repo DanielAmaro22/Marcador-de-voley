3bc27e363bfc12ed6086ca698268a8a941cf0fa0
DanielAmaro22 <cdanielamaro22@gmail.com>
2026-04-19 07:34:06 -0300

    Update scoring logic for teams A and B

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let scoreA = 0
 let scoreB = 0
+let setA = 0
+let setB = 0
 
 function addPoint(team){
   
@@ -7,17 +9,24 @@ function addPoint(team){
     scoreA ++;
     document.getElementById("scoreA").textContent = scoreA;
   }
-   else if (team === 'B'){
+  else if (team === 'B'){
     scoreB ++;
     document.getElementById("scoreB").textContent = scoreB;
   }
-  if (scoreA >=25 && scoreA - scoreB >1){
-    alert("Ganador team A!")
+  if (scoreA >=25 && scoreA - scoreB >=2){
+    setA++;
+    alert("Set para team A!");
+    document.getElementById("setA").textContent= setA;
+    resetScore();
+    return;
   }
-    if (scoreB >=25 && scoreB - scoreA >1)
-    alert("Ganador team B!")
+    if (scoreB >=25 && scoreB - scoreA >=2){
+    setB++;
+    alert("Set para team B!");
+    document.getElementById("setB").textContent= setB;
+    resetScore();
+    return;
 }
-
 function subtractPoint(team){
   if (team === "A" && scoreA > 0){
     scoreA --;
